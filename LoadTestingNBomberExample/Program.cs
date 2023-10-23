9f398ab8322d01c9714721488f623675aefbbd91
djesusnet13 <djesus.net@gmail.com>
2023-10-23 07:52:54 -0300

    Subida do projeto de teste de carga

---
+++ LoadTestingNBomberExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using NBomber.CSharp;
+
+using var httpClient = new HttpClient();
+
+var scenario = Scenario.Create("Meu Primeiro Teste Carga", async context =>
+    {
+        var response = await httpClient.GetAsync("https://nbomber.com");
+
+        return response.IsSuccessStatusCode
+            ? Response.Ok()
+            : Response.Fail();
+    })
+    .WithoutWarmUp()
+    .WithLoadSimulations(
+        Simulation.Inject(rate: 10,
+            interval: TimeSpan.FromSeconds(1),
+            during: TimeSpan.FromSeconds(30))
+    );
+    
+    NBomberRunner
+        .RegisterScenarios(scenario)
+        .Run();
